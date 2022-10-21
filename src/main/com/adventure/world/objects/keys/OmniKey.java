c89f9d356ea26de40cd9ef97bb10ae3d2aced88a
Juan Ruiz <juanruiz@Juans-MacBook-Pro.local>
2022-10-21 15:57:43 -0700

    fixed styling

--- src/main/com/adventure/world/objects/keys/OmniKey.java
+++ src/main/com/adventure/world/objects/keys/OmniKey.java
@@ -27,9 +27,14 @@ public class OmniKey implements Tangible {
     @Override
     public void useItem(Tangible door) {
         if (door instanceof OmniDoor) {
-            //TODO Complete the function
-            System.out.println("Your solution goes here");
+            OmniDoor door1 = (OmniDoor) door;
+            for (int i = 0; i < pinLength; i++) {
+                pins[i] = door1.pins[i];
+            }
+        } else {
+            System.out.println("This key can only be used with OmniDoors!");
         }
+
     }
 
     @Override
